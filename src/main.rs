29c88f59e7e106e25ee5ebc709b1b998bb9f0a78
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2020-11-19 03:43:54 -0700

    adjust res based on monitor resolution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use winit::event_loop::EventLoop;
+
 use bracket_lib::prelude::*;
 
 use text_rts::State;
@@ -5,17 +7,22 @@ use text_rts::State;
 fn main() -> BError {
     let tw = 24;
     let th = 24;
-    let w = 1366 / tw;
-    let h = 768 / th;
 
-    let ctx = BTermBuilder::simple(w, h)?
+    let size = if let Some(monitor) = EventLoop::new().available_monitors().next() {
+        (monitor.size().width / tw, monitor.size().height / th)
+    } else {
+        (1366 / tw, 768 / th)
+    };
+    println!("{:?}", size);
+
+    let ctx = BTermBuilder::simple(size.0, size.1)?
         .with_tile_dimensions(tw, th)
         .with_advanced_input(true)
         .with_fps_cap(60.0)
         .with_fullscreen(true)
         .with_title("TextRTS")
         .build()?;
-    let gs = State::new(w - 1, h);
+    let gs = State::new(size.0 - 1, size.1);
 
     main_loop(ctx, gs)
 }
